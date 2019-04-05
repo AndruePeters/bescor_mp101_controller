f85a2d440c1624ffd99aacdce7cf47f241bfab05
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-04-05 15:28:54 -0400

    Formatting changes

--- code/shared/node/node.h
+++ code/shared/node/node.h
@@ -136,12 +136,13 @@ rf24_datarate_e node_get_data_rate(struct node_prop *np);
   Sets the crc length of the radio.
   typedef enum { RF24_CRC_DISABLED = 0, RF24_CRC_8, RF24_CRC_16 } rf24_crclength_e;
 */
-void node_set_crc_length(struct node_prop *np, rf24_crclength_e);
+void node_set_crc_length(struct node_prop *np, rf24_crclength_e cl);
 
 /*
   Returns the crc length.
 */
 rf24_crclength_e node_get_crc_length(struct node_prop *cp);
+
 /*
   Sets the channel the node operates on.
 */
