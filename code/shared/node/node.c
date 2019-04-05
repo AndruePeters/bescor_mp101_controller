25bf7161225f0176cc9b8946d9e05d53e4899c0b
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-04-05 15:02:39 -0400

    Added ndoe_set_color

--- code/shared/node/node.c
+++ code/shared/node/node.c
@@ -32,3 +32,18 @@ void node_init(struct node_prop *np)
 {
   assert(np != NULL);
 }
+
+
+/*
+  Sets the rgb color of the node.
+  If it's not a valid value, then it is set to be OFF.
+*/
+void node_set_color(struct node_prop *np, color_e c)
+{
+  assert(np != NULL);
+  if (c < 0 || c >= NUM_COLORS) {
+    np->color = OFF;
+  } else {
+    np->color = c;
+  }
+}
