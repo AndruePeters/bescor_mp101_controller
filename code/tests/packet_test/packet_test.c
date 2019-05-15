9c1252a5e8a8b2212831bacd3b24fbf329afa004
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-05-15 17:03:10 -0400

    edited

--- code/tests/packet_test/packet_test.c
+++ code/tests/packet_test/packet_test.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
+
 #include "packet.h"
 #include "packet.c"
 int main()
 {
+  printf("\nBeginning tests\n\n");
   struct packet p;
   uint8_t data[4] = {5, 6, 7, 8};
   packet_init(&p);
@@ -9,5 +12,6 @@ int main()
   packet_set_ID(&p, 4);
   packet_set_payload_used(&p, 20);
   packet_set_payload_data(&p, data, 6);
+  printf("\n\nEnding tests.\n\n");
   return 0;
 }
