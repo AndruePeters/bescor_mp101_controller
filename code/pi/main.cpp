f72d08566647e073787bca6a0a92de02b161092e
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-06-01 22:56:27 -0400

    Added global  instance

--- code/pi/main.cpp
+++ code/pi/main.cpp
@@ -3,7 +3,10 @@
 #include <wiringPi.h>
 #include <RF24.h>
 
-
+// global radio instance
+// using wiringPi, so wiringPi pin scheme is also used
+// this is equivalent to radio(22, 0) in the generic gettingstarted.cpp
+RF24 radio(3, 10);
 
 int main()
 {
