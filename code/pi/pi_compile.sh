5cc2e0a2fde82387f8fcf98823452b11a2c4c360
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-06-02 09:36:18 -0400

    Builds and transfrs to pi on network

--- code/pi/pi_compile.sh
+++ code/pi/pi_compile.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 export RPI_TC_PATH=~/Development/pi/pi_toolchain
+rm -r _build
 cmake . -DCMAKE_TOOLCHAIN_FILE=toolchain-rpi.cmake -B_build
+cd _build
+make
+scp main pi@192.168.1.162:~/Desktop
+cd ..
+
