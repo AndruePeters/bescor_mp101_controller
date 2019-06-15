5e94c611e1ab97b117f4eae063bb446881d4766d
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-06-15 16:39:03 -0400

    Initial Arduino commit

---
+++ code/arduino/include/packet.h
@@ -0,0 +1,1 @@
+../../shared/packet/packet.h
---
+++ code/arduino/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <RF24.h>
+#include <SPI.h>
+#include <IRLibSendBase.h>
+#include <IRLib_P01_NEC.h>
+#include <IRLib_P02_Sony.h>
+#include <IRLibCombo.h>
+
+#include "node.h"
+#include "packet.h"
+
+int main()
+{
+
+}
