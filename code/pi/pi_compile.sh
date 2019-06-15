e2b84b3b7642e62e80c4004f668c1f7e0dc3f073
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-06-15 01:31:58 -0400

    Tests main.cpp automatically on Pi

--- code/pi/pi_compile.sh
+++ code/pi/pi_compile.sh
@@ -4,14 +4,11 @@ rm -r _build
 cmake . -DCMAKE_TOOLCHAIN_FILE=toolchain-rpi.cmake -B_build
 cd _build
 make
-scp main pi@192.168.1.162:~/Desktop
+scp main pi@192.168.1.162:~
 cd ..
-<<<<<<< HEAD
 echo Done
-=======
-ssh  -tt pi@192.168.1.162 << 'ENDSSH'
-  echo "Trying to run main"
-  ~/Desktop/main
+ssh  -tt pi@192.168.1.162 << ENDSSH
+  cd ~/Desktop
+  ./main
   echo "Finished running main"
 ENDSSH
->>>>>>> 589d21e512b96819e1c8235c24e252be09e9cd42
