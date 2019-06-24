a3f83622c30a5ae8252dbf818d101143e2302230
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-06-24 12:20:50 -0400

    Moved paths

---
+++ code/pi/js.h
@@ -0,0 +1,105 @@
+#ifndef _BESCOR_MP101_CONTROLLER_JS_
+#define _BESCOR_MP101_CONTROLLER_JS_
+
+#include <array>
+#include <string>
+#include <bitset>
+#include <SFML/Window/Joystick.hpp>
+const int AXIS_INVERTED = -1;
+const int AXIS_NORMAL = 1;
+
+typedef unsigned Button;
+typedef sf::Joystick::Axis Axis;
+
+enum INV_AXIS {
+  X_INV = 0, Y_INV, Z_INV, R_INV, U_INV, V_INV, PovX_INV, PovY_INV
+};
+
+/*
+  Wrapper class for SFML
+*/
+class JS_State {
+public:
+  JS_State(unsigned js_num);
+  void update();
+  bool setJSNum(unsigned js_num);
+  void waitUntilConnected();
+  unsigned getJSNumber() const;
+  unsigned getNumBtns() const;
+  bool isConnected() const;
+  bool isBtnPressed(Button btn) const;
+  float getAxisPos(Axis axis) const;
+  void invertX(bool inv);
+  void invertY(bool inv);
+  void invertR(bool inv);
+  void invertZ(bool inv);
+  void invertU(bool inv);
+  void invertV(bool inv);
+  void invertPovX(bool inv);
+  void invertPovY(bool inv);
+  std::string getName();
+  unsigned getVendorID();
+  unsigned getProductID();
+
+private:
+  unsigned js_num;
+
+  std::array <int, 8> inv_axes_state;
+  void printJsNotConnected() const;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
