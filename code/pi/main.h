4a550085e86f6580c65de1ace8808603d1419a87
Drue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-07-05 14:48:44 -0400

    Removed ir protocol define statements and updated node.h ir_prot_e

--- code/pi/main.h
+++ code/pi/main.h
@@ -5,26 +5,6 @@
 
 #include <list>
 
-
-/*
-  These are defined in IRLIB2 for the Arduino platform, and are
-  used here for conistency.
-*/
-#define UNKNOWN 0
-#define NEC 1
-#define SONY 2
-#define RC5 3
-#define RC6 4
-#define PANASONIC_OLD 5
-#define JVC 6
-#define NECX 7
-#define SAMSUNG36 8
-#define GICABLE 9
-#define DIRECTV 10
-#define RCMM 11
-#define CYKM 12
-
-
 /*
  *  node_list_t is the container to store each camera node
  *  Uses a doubly linked list:
@@ -34,6 +14,7 @@
  */
 typedef std::list<node_prop *> node_list_t;
 typedef std::list<node_prop *>::iterator node_list_it;
+
 
 /*
  *  node_list is the global variable to store nodes.
