d9bb9bb4bf392f0aaf788dab1e54fb07b9be3a46
Drue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-09-22 22:28:39 -0400

    Doesn't autorun

--- code/pi/pi_compile.sh
+++ code/pi/pi_compile.sh
@@ -5,8 +5,8 @@ cmake . -DCMAKE_TOOLCHAIN_FILE=toolchain-rpi.cmake -B_build
 cd _build
 make
 scp main pi@192.168.1.162:~
-scp ../config.yaml pi@192.168.1.162:~
-cd ..
+cd .. 
+scp config.yaml pi@192.168.1.162:~
 echo Done
 ssh  -tt -T pi@192.168.1.162 << 'ENDSSH'
   ./main
