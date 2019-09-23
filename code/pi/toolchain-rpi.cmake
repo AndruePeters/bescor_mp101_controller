a9de913cb5aa0c4334d68f245715e1901e6718d1
Drue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-09-22 22:29:01 -0400

    Uses g++-8

--- code/pi/toolchain-rpi.cmake
+++ code/pi/toolchain-rpi.cmake
@@ -5,7 +5,7 @@ SET(CMAKE_SYSTEM_VERSION 1)
 #SET(CMAKE_C_COMPILER $ENV{RPI_TC_PATH}/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc)
 #SET(CMAKE_CXX_COMPILER $ENV{RPI_TC_PATH}/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-g++)
 SET(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
-SET(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+SET(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++-8)
 
 
 # set toolchain path using environmental variables
